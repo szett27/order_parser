db545bc30f2eb8ed822b1cfdca6d0bd2b98b28dc
Scott <scott.zetterstrom@gmail.com>
2021-02-07 11:41:35 +0300

    added initial csv parser

---
+++ csvparser.js
@@ -0,0 +1,16 @@
+//parse js arrays to cs
+
+
+let test_arr = [['1.1 All Components', '1.2 Do this'], ['1.2 AFCENT', '1.3 Move Stuff here']];
+
+let csv_parser = (arr1) =>{
+
+    console.log(test_arr)
+
+
+}
+
+
+csv_parser(test_arr);
+
+
