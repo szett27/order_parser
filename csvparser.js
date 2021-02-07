b65ebc1bb3824abe8b18c523af8f5a3c1487c858
Scott <scott.zetterstrom@gmail.com>
2021-02-07 13:24:24 +0300

    csv parser can take an array and write a csv file

--- csvparser.js
+++ csvparser.js
@@ -6,7 +6,17 @@ let test_arr = [['1.1 All Components', '1.2 Do this'], ['1.2 AFCENT', '1.3 Move 
 let csv_parser = (arr1) =>{
 
     console.log(test_arr)
+    var fs = require('fs');
 
+    var file = fs.createWriteStream('order.csv');
+    file.on('error', function(err) { /* error handling */ });
+    test_arr.forEach(function(v) {
+        console.log(v)
+         file.write(v.join(', ') + '\n'); 
+        
+        });
+
+    file.end();
 
 }
 
